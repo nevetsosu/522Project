cc9cd9e986e84307e2037e1dd3b18e37cbc423c2
Steven Dao <stevendao100@gmail.com>
2024-12-08 01:01:22 -0500

    rename  test to process

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 # custom modules
 from loader import load
-from reduce import autoencode
+from reduce import autoencode, pca, lda
 from preprocess import fill_missing, display_missing, encode, standardize
-from test import test_DTree, test_LGBM, test_RForest
+from process import DTree, LGBM, RForest
 from tsne import display_tsne
 
 def main():
+    TEST_SIZE = 0.2
     X, Y = load('train.csv', nrows=1000000)
 
     # preprocess
-    X = fill_missing(X)                                                                                         # fill NaNs
-    X = encode(X)                                                                                               # categories -> numbers
-    X = standardize(X)                                                                                          # standardization u=0, std=1
+    X = fill_missing(X)         # fill NaNs
+    X = encode(X)               # categories -> numbers
+    X = standardize(X)          # standardization u=0, std=1
 
     # tests without dimensionality reduction
-    test_DTree(X, Y, test_size=0.2)
-    test_LGBM(X, Y, test_size=0.2)
-    # test_RForest(X_scaled, Y, test_size=0.2)
+    DTree(X, Y, test_size=TEST_SIZE)
+    LGBM(X, Y, test_size=TEST_SIZE)
+    # test_RForest(X_scaled, Y, test_size=TEST_SIZE)
 
     # Dimensionality reduction
-    REDUCED_DIMS = 40
-    X_low = autoencode(X, n_components=REDUCED_DIMS, save=True, force_refit=False, epochs=50, batch_size=256, shuffle=True)
+    # REDUCED_DIMS = 40
+    # X = autoencode(X, n_components=REDUCED_DIMS, save=True, force_refit=False, epochs=50, batch_size=256, shuffle=True)
+    # X = pca(X, n_components=0.95)
+    X = lda(X, Y)
 
     # test post-dimensionality reduction
-    test_DTree(X_low, Y, test_size=0.2)
-    test_LGBM(X_low, Y, test_size=0.2)
+    DTree(X, Y, test_size=TEST_SIZE)
+    LGBM(X, Y, test_size=TEST_SIZE)
 
 
 if __name__ == '__main__':
