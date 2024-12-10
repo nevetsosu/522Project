8484df45135ada5d59f4f46b552abfe47bdd6aa7
Jonathan Tran <jonathan.tran227@gmail.com>
2024-12-10 04:09:15 -0500

    update process.py

--- process.py
+++ process.py
@@ -23,12 +23,12 @@ def DTree(X, Y, test_size, **kwargs):
     print("[TEST] Testing Decision Tree")
     X_train, X_val, y_train, y_val = train_test_split(X, Y, test_size=test_size, random_state=42, **kwargs)
 
-    # LightGBM model for standard scaled data
-    lgb_model_scaled = DecisionTreeClassifier(random_state=42)
-    lgb_model_scaled.fit(X_train, y_train)
+    # Decision Tree model for standard scaled data
+    dtree_model_scaled = DecisionTreeClassifier(random_state=42)
+    dtree_model_scaled.fit(X_train, y_train)
 
     # Make predictions on validation set
-    y_pred_scaled = lgb_model_scaled.predict_proba(X_val)[:, 1]
+    y_pred_scaled = dtree_model_scaled.predict_proba(X_val)[:, 1]
     auc_scaled = roc_auc_score(y_val, y_pred_scaled)
     return auc_scaled
 
@@ -36,12 +36,12 @@ def RForest(X, Y, test_size, n_estimators=100, **kwargs):
     print("[TEST] Testing Random Forest")
     X_train, X_val, y_train, y_val = train_test_split(X, Y, test_size=test_size, random_state=42)
 
-    # LightGBM model for standard scaled data
-    lgb_model_scaled = RandomForestClassifier(n_estimators, n_jobs=-1, random_state=42, **kwargs)
-    lgb_model_scaled.fit(X_train, y_train)
+    # Random Forest model for standard scaled data
+    rf_model_scaled = RandomForestClassifier(n_estimators, n_jobs=-1, random_state=42, **kwargs)
+    rf_model_scaled.fit(X_train, y_train)
 
     # Make predictions on validation set
-    y_pred_scaled = lgb_model_scaled.predict_proba(X_val)[:, 1]
+    y_pred_scaled = rf_model_scaled.predict_proba(X_val)[:, 1]
     auc_scaled = roc_auc_score(y_val, y_pred_scaled)
     return auc_scaled
 
@@ -50,11 +50,11 @@ def MLP(X, Y, test_size, hidden_layer_sizes=(100,), **kwargs):
     print("[TEST] Testing MLP")
     X_train, X_val, y_train, y_val = train_test_split(X, Y, test_size=test_size, random_state=42, **kwargs)
 
-    # Train MLP
-    mlp_model = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, random_state=42)
-    mlp_model.fit(X_train, y_train)
+    # Train MLP for standard scaled data
+    mlp_model_scaled = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, random_state=42)
+    mlp_model_scaled.fit(X_train, y_train)
 
     # Make predictions on validation set
-    y_pred_scaled = mlp_model.predict_proba(X_val)[:, 1]
+    y_pred_scaled = mlp_model_scaled.predict_proba(X_val)[:, 1]
     auc_scaled = roc_auc_score(y_val, y_pred_scaled)
     return auc_scaled
