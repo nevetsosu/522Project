176dc08bb199f8d0ea990dc6f43e9f122bf6b6a4
Jonathan Tran <jonathan.tran227@gmail.com>
2024-12-10 16:07:40 -0500

    fusion

---
+++ fusion.py
@@ -0,0 +1,20 @@
+from sklearn.metrics import roc_auc_score
+import numpy as np
+
+
+def fusion(prob_predictions, y, weights=None, **kwargs):
+    prob_predictions = np.array(prob_predictions)
+
+    if weights is None:
+        weights = [1] * len(prob_predictions)  
+    
+    if (sum(weights) == 0):
+        weight_sum = 1
+    else:
+        weight_sum = sum(weights)
+    weights = np.array(weights) / weight_sum  # normalize weights
+    fused_probs = np.average(prob_predictions, axis=0, weights=weights)
+
+    auc_fused = roc_auc_score(y, fused_probs)
+    print(f"[FUSION] AUC after fusion: {auc_fused:.4f}")
+    return auc_fused
